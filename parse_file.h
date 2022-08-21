257ae60ff499d7f33d311b2d7c77431f67c155dd
KayTuval <106312981+KayTuval@users.noreply.github.com>
2022-08-21 23:19:55 +0300

    Update parse_file.h

--- parse_file.h
+++ parse_file.h
@@ -1,1 +1,21 @@
 
+#ifndef PARSE_FILE_H
+#define PARSE_FILE_H
+
+#include <stdio.h>
+
+//student struct
+typedef struct student {
+    char id[10];
+    char password[256];
+    char grade[4];
+} student_t;
+
+//TA struct
+typedef struct TA {
+    char id[10];
+    char password[256];
+} TA_t;
+
+
+#endif // PARSE_FILE_H
